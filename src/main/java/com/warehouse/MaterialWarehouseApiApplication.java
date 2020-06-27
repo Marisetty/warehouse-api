5c12a34a39c2e5c0e5bd0911cf2ef72d99e9c607
Marisetty <38437604+Marisetty@users.noreply.github.com>
2020-06-27 16:08:09 +0530

    msaikri : Moving the initial commit

---
+++ src/main/java/com/warehouse/MaterialWarehouseApiApplication.java
@@ -0,0 +1,13 @@
+package com.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialWarehouseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialWarehouseApiApplication.class, args);
+	}
+
+}
